f0d80ecf5d953071d0d97ba2e0518233f2312fee
Zeke Medley <zekemedley@gmail.com>
2020-08-02 17:54:57 -0700

    Select randomly from full page range

--- pages/api/nyt.js
+++ pages/api/nyt.js
@@ -2,6 +2,6 @@ const apiKey = process.env.NYT_API_KEY;
 const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=102&api-key=${apiKey}`;
 
 export default async (req, res) => {
-    let url = queryUrl + `&page=${Math.floor(Math.random()*10)}`;
+    let url = queryUrl + `&page=${Math.floor(Math.random()*200)}`;
     await fetch(url).then(resp => resp.json()).then(data => res.json(data));
 }
